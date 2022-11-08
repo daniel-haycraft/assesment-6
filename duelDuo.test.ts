bd428ec03a40def68091f4b4ce19218e31edf41a
Daniel Haycraft <dhcopy1@gmail.com>
2022-11-08 13:45:01 -0700

    working

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,12 +17,24 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBeTruthy();
-
-    
     await driver.sleep(3000);
-})
+});
 
-test('adds one to winners or lossers', async () => {
-    
+test('does it click?', async () => {
+    const seeTheTimmys = await driver.findElement(By.id('see-all'))
+    const timmyClick = await seeTheTimmys.click()
+    // i don't need to re reference timmyclick bc i just want it to click then check if it displayed the bots 
+    const timsABot = await driver.findElement(By.id('all-bots'))
+    const timDisplayed = await timsABot.isDisplayed()
+    expect(timDisplayed).toBeTruthy();
     await driver.sleep(3000);
+});
+
+test('not quite sure yet', async () => {
+    const draw = await driver.findElement(By.id('draw')).click()
+    const options = await driver.findElement(By.id('choices'))
+    const bots = await options.isDisplayed()
+    expect(bots).toBe(true)
+    await driver.sleep(3000);
+
 })
